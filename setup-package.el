2db084c568627890e31b58d39cf5e6d0499b49fa
Quan Ta <qta@federatedmedia.net>
2013-06-14 13:05:26 -0700

    Fix dependencies for clj-defuns.el

--- setup-package.el
+++ setup-package.el
@@ -60,10 +60,13 @@
    (cons 'paredit melpa)
    (cons 'shell-command melpa)
    (cons 'slime-js marmalade)
+   (cons 's melpa)
+   (cons 's-buffer melpa)
    (cons 'smooth-scrolling melpa)
    (cons 'smex melpa)
    (cons 'yasnippet marmalade)
    (cons 'yasnippet-bundle marmalade)
+   (cons 'zen-mode marmalade)
    (cons 'zencoding-mode marmalade)
    ))
 
